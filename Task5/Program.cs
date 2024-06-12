27aa65628b7dd5abab56d539eb0c8e25209d4a46
Dmitry Ismailov <dmitriyismailov20@mail.ru>
2024-06-12 18:01:01 +0300

    Сравнили числа с пробелами

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+int a = 100;
+int b = 1;
+
+while (b <= a)
+{
+    Console.Write(b);
+    Console.Write(' ');
+    b = b + 1;
+}
